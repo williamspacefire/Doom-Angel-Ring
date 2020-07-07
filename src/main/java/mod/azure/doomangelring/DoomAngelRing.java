56eedd94ccdcdeca01e2331207455b1e310fdd9d
AzureZhen <rcraft@cyberconnects2.com>
2020-07-07 00:59:58 -0400

    v1.0.1 Ring slot now works

--- src/main/java/mod/azure/doomangelring/DoomAngelRing.java
+++ src/main/java/mod/azure/doomangelring/DoomAngelRing.java
@@ -1,13 +1,14 @@
 package mod.azure.doomangelring;
 
-import mod.azure.doomangelring.compat.CuriosCompat;
 import mod.azure.doomangelring.register.ItemRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 
 @Mod("doomangelring")
 public class DoomAngelRing {
@@ -16,11 +17,12 @@ public class DoomAngelRing {
 	public DoomAngelRing() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		MinecraftForge.EVENT_BUS.register(this);
+		modEventBus.addListener(this::enqueueIMC);
 		ItemRegistry.ITEMS.register(modEventBus);
 	}
 
-	@SubscribeEvent
-	public static void sendImc(InterModEnqueueEvent event) {
-		CuriosCompat.sendImc();
+	private void enqueueIMC(InterModEnqueueEvent event) {
+		InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE,
+				() -> SlotTypePreset.RING.getMessageBuilder().build());
 	}
 }
--- src/main/java/mod/azure/doomangelring/ring/ItemDoomAngelRing.java
+++ src/main/java/mod/azure/doomangelring/ring/ItemDoomAngelRing.java
@@ -1,21 +1,96 @@
 package mod.azure.doomangelring.ring;
 
-import mod.azure.doomangelring.compat.CuriosCompat;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import mod.azure.doomangelring.register.ItemRegistry;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class ItemDoomAngelRing extends Item {
 
 	public ItemDoomAngelRing() {
-		super(new Item.Properties().maxStackSize(1).group(ItemGroup.MISC));
+		super(new Item.Properties().maxStackSize(1).group(ItemGroup.MISC).defaultMaxDamage(500));
 	}
 
 	@Override
 	public ICapabilityProvider initCapabilities(final ItemStack stack, CompoundNBT unused) {
+		ICurio curio = new ICurio() {
+			@Override
+			public boolean canRightClickEquip() {
+				return true;
+			}
 
-		return CuriosCompat.initCapabilities();
+			@Override
+			public void onEquip(String identifier, int index, LivingEntity livingEntity) {
+				if (livingEntity instanceof PlayerEntity) {
+					startFlying((PlayerEntity) livingEntity);
+				}
+			}
+
+			@Override
+			public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
+				if (livingEntity instanceof PlayerEntity) {
+					stopFlying((PlayerEntity) livingEntity);
+				}
+			}
+
+			private void startFlying(PlayerEntity player) {
+				if (!player.isCreative() && !player.isSpectator()) {
+					player.abilities.allowFlying = true;
+					player.sendPlayerAbilities();
+				}
+			}
+
+			private void stopFlying(PlayerEntity player) {
+				if (!player.isCreative() && !player.isSpectator()) {
+					player.abilities.isFlying = false;
+					player.abilities.allowFlying = false;
+					player.sendPlayerAbilities();
+				}
+			}
+
+			@Override
+			public void curioTick(String identifier, int index, LivingEntity livingEntity) {
+				if (livingEntity instanceof PlayerEntity) {
+					PlayerEntity player = ((PlayerEntity) livingEntity);
+					if (!player.abilities.allowFlying) {
+						startFlying(player);
+					}
+				}
+			}
+
+			@Override
+			public boolean canEquip(String identifier, LivingEntity entityLivingBase) {
+				return !CuriosApi.getCuriosHelper().findEquippedCurio(ItemRegistry.RING.get(), entityLivingBase)
+						.isPresent();
+			}
+		};
+
+		return new ICapabilityProvider() {
+			private final LazyOptional<ICurio> curioOpt = LazyOptional.of(() -> curio);
+
+			@Nonnull
+			@Override
+			public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+
+				return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
+			}
+		};
+	}
+
+	public static boolean isRingInCuriosSlot(ItemStack ring, LivingEntity player) {
+		return CuriosApi.getCuriosHelper().findEquippedCurio(ring.getItem(), player).isPresent();
 	}
 }
